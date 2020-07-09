e1a1259c9d02ca235667f2861ed893cd77ee1c2c
Jason Wright <jwright@speareducation.com>
2020-07-09 12:21:41 -0700

    updated entrypoint to be more dynamic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 [ -z "$AWS_REGION" ] && [ ! -z "$INPUT_AWS_REGION" ] &&\
-    AWS_REGION="$INPUT_AWS_REGION"
+    export AWS_REGION="$INPUT_AWS_REGION"
 
-[ -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$INPUT_AWS_ACCESS_KEY_ID" ] &&\ 
-    AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
+[ -z "$AWS_ACCESS_KEY_ID" ] && [ ! -z "$INPUT_AWS_ACCESS_KEY_ID" ] &&\
+    export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
 
-[ -z "$AWS_SECRET_ACCESS_KEY" ] && [ ! -z "$INPUT_AWS_SECRET_ACCESS_KEY" ] &&\ 
-    AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
+[ -z "$AWS_SECRET_ACCESS_KEY" ] && [ ! -z "$INPUT_AWS_SECRET_ACCESS_KEY" ] &&\
+    export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 
 INITIAL_DIR=$(pwd)
 
-DATABASES=${DATABASES:-$1}
-[ -z "$DATABASES" ] && echo '$DATABASES Not set' && exit 1
+[ -z "$INPUT_DATABASES" ] && echo '$INPUT_DATABASES Not set' && exit 1
 
-mkdir -p .drone/databases
-cd .drone/databases
+mkdir .core-action-database-setup && cd .core-action-database-setup
+
 echo "
 [mysql]
 user = $INPUT_MYSQL_USER
@@ -25,35 +24,33 @@ host = $INPUT_MYSQL_HOST
 [ ! -z "$INPUT_MYSQL_PASS" ] && echo "pass = $INPUT_MYSQL_PASS" >> .my.cnf
 [ ! -z "$INPUT_MYSQL_PORT" ] && echo "port = $INPUT_MYSQL_PORT" >> .my.cnf
 
-
 MYSQL="mysql --defaults-file=.my.cnf"
-
-for DB in $DATABASES
-do 
-    aws s3 cp s3://spear-backup/schemas/$DB.latest.sql.gz ./; 
-done
 
 # Wait for MySQL to start"
 i=0; while [ $((i+1)) -lt 30 ] && [ ! $($MYSQL -Nse "SELECT VERSION();") ]
 do
     echo "Waiting for MySQL... $i"
-    sleep 1; 
+    sleep 1;
+done
+[ "$i" == "30" ] && echo "Failed to connect to mysql." && exit 1
+
+for ENTRY in $(echo "$INPUT_DATABASES" | jq -c .[])
+do
+    db=$(echo "$ENTRY" | jq -r .db)
+    s3Uri=$(echo "$ENTRY" | jq -r .s3Uri)
+    dumpFile=$(basename "$s3Uri")
+    aws s3 cp "$s3Uri" "./$dumpFile"
+
+    echo "Creating $db"
+    $MYSQL -e "DROP DATABASE IF EXISTS $db; CREATE DATABASE $db;" || exit 1
+    
+    echo "Importing $db"
+    if [[ "$dumpFile" == *.gz ]]
+    then
+        gunzip -c "$dumpFile" | $MYSQL $db
+    else
+        cat "$dumpFile" | $MYSQL $db
+    fi
 done
 
-for DB in $DATABASES
-do 
-    DB_TDD="$DB"_tdd
-    echo "Dropping $DB_TDD"
-    $MYSQL -e "DROP DATABASE IF EXISTS $DB_TDD; CREATE DATABASE $DB_TDD;"
-done
-
-for DB in $DATABASES
-do 
-    DB_TDD="$DB"_tdd
-    echo "Importing $DB_TDD"
-    gunzip -c $DB.latest.sql.gz | $MYSQL $DB_TDD
-done
-
-cd $INITIAL_DIR
 exit 0
-
