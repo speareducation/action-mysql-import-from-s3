8281d6718ca61419ccf4d2b157c0951ee8615920
Kenzi Stewart <kstewart@dployr.com>
2021-11-05 09:52:23 -0700

    Remove unneeded cleanup when moving to tmp dir.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM spear/pipeline:latest
 COPY entrypoint.sh /entrypoint.sh
-COPY cleanup.sh /cleanup.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-S3_IMPORTS_DIR=/tmp/.mysql-import-from-s3
-
-echo "Cleaning up .mysql-import-from-s3 dir..."
-rm -rf ./.mysql-import-from-s3
-exit 0
